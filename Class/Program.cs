35e014e409bddea7b261c5771da5bea9bab305f7
Sonava <sonava@code.edu.az>
2023-10-14 16:13:47 +0400

    Task tamamlandı

---
+++ Class/Program.cs
@@ -0,0 +1,18 @@
+﻿//Task:
+//Student classi yaradirsiz.Name, Surname, Group, Point, isGraduated
+//(mezun olub olmadigi ile bagli boolean bir type olacaq) field-leri olacaq.
+//Student classindan instance alindigi zaman birbasha student field-lerinin 
+//deyerleri dinamik sekilde teyin olunsun.
+//Studentin Ad ve soyadini geriye qaytaran bir method yazirsiz.
+//Studentin butun deyerlerini geriye qaytaran bir method yazirsiz. 
+//Eger mezun olubsa butun deyerlerden sonra bu telebe mezun olub deye yazilsin,
+//eger mezun olmayibsa mezun olmadigi ile bagli bir melumat yazilsin
+//Studentin balini artirmaq ucun novbeti imtahana girib gire bilmeyeceyi ile
+//bagli bir melumat geriye qaytaran method yazirsiz. Bali 80-den yuxari 
+//telebeler ikinci imtahana gire biler, asagi olan telebeler ise gire bilmezler.
+
+Student student = new Student("Sona","Abbasova","P237",100,false);
+
+student.ShowFullName();
+student.ShowFullInfo();
+student.SecondChance();
---
+++ Class/Student.cs
@@ -0,0 +1,50 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+public class Student
+{
+    public string Name;
+    public string Surname;
+    public string Group;
+    public int Point;
+    public bool IsGraduated;
+    public Student(string name, string surname, string group, int point, bool isGraduated)
+    {
+        Name = name;
+        Surname = surname;
+        Group = group;
+        Point = point;
+        IsGraduated = isGraduated;
+    }
+    public void ShowFullName()
+    {
+        Console.WriteLine($"FullName = {Name} {Surname}");
+    }
+    public void ShowFullInfo()
+    {
+        Console.WriteLine($"Name = {Name} Surname = {Surname} Group = {Group} Point = {Point} IsGraduated = {IsGraduated}");
+
+        if (IsGraduated == true)
+        {
+            Console.WriteLine("Telebe artiq mezun olub.");
+        }
+        else {
+            Console.WriteLine("Telebe hele mezun olmayib.");
+        }
+    }
+
+    public void SecondChance()
+    {
+        if (Point>80 && Point<100)
+        {
+            Console.WriteLine("2-ci sans imtahaninda istirak ucun haqq qazanmisiniz.");
+        }
+        else if(Point<80)
+        {
+            Console.WriteLine("2-ci sans imtahaninda istirak haqqini itirdiniz.");
+        }
+        else
+        {
+            Console.WriteLine("Siz ilk imtahanda maksimum netice gostermisiniz.");
+        }
+    }
+}
